c921f6e26fabcfb6b9ebcf94b6060103fe280379
pp19dd <pp19dd@gmail.com>
2016-03-14 11:45:29 -0400

    Move smarty modifier function ago to composer package

--- index.php
+++ index.php
@@ -3,20 +3,6 @@
 require_once( 'config.php' );
 require_once( 'vendor/autoload.php' );
 
-function smarty_modifier_ago($tm, $cur_tm = null) {
-	if( is_null($cur_tm) ) $cur_tm = time();
-	$dif = $cur_tm-strtotime($tm);
-
-	$pds = array('second','minute','hour','day','week','month','year','decade');
-	$lngh = array(1,60,3600,86400,604800,2630880,31570560,315705600);
-	for($v = sizeof($lngh)-1; ($v >= 0)&&(($no = $dif/$lngh[$v])<=1); $v--);
-	if($v < 0) $v = 0;
-	$_tm = $cur_tm-($dif%$lngh[$v]);
-	$no = floor($no); if($no <> 1) $pds[$v] .='s'; $x=sprintf("%d %s ",$no,$pds[$v]);
-
-	return $x;
-}
-
 // load sample data for the mockup
 #include( 'data-sample.php' );
 date_default_timezone_set( 'America/New_York' );
